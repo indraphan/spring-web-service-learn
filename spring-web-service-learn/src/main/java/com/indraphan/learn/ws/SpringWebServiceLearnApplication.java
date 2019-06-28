64444f21b921bf70fdeb5b6b82c7e92a13465660
indraphan <indra.phan@gmail.com>
2019-06-28 23:38:31 +0800

    Initial Commit

---
+++ spring-web-service-learn/src/main/java/com/indraphan/learn/ws/SpringWebServiceLearnApplication.java
@@ -0,0 +1,13 @@
+package com.indraphan.learn.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebServiceLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebServiceLearnApplication.class, args);
+	}
+
+}
